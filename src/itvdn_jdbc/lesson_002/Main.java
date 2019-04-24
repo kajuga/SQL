118c366bedf54ce63f4a45ca73279f4f31d4a43b
kajuga <Ff14789kldd>
2019-04-24 14:43:51 +0300

    ITVDN JDBC course 24.04.19

--- src/itvdn_jdbc/lesson_002/Main.java
+++ src/itvdn_jdbc/lesson_002/Main.java
@@ -1,28 +1,26 @@
 package itvdn_jdbc.lesson_002;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.Buffer;
 import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-        String url = "jdbc:mysql://localhost:3306/itvdn_jdbc";
+        String url = "jdbc:mysql://localhost:3306/first_lesson";
         String userName = "root";
-        String pass = "pass";
-
+        String password = "pass";
         Class.forName("com.mysql.jdbc.Driver");
 
-        try (Connection connection = DriverManager.getConnection(url, userName, pass);
-             BufferedReader sqFile = new BufferedReader(new FileReader("/home/kajuga/projects/SQL/src/itvdn_jdbc/approach.sql"));
-             Scanner scanner = new Scanner(sqFile);
-             Statement statement = connection.createStatement()){
+        try (Connection connection = DriverManager.getConnection(url, userName, password);
+        BufferedReader sqlFile = new BufferedReader(new FileReader("/home/kajuga/projects/SQL/src/itvdn_jdbc/lesson_002/approach.sql"));
+        Scanner scan = new Scanner(sqlFile);
+        Statement statement = connection.createStatement()) {
+
             String line = "";
-            while (scanner.hasNextLine()) {
-                line = scanner.nextLine();
+            while (scan.hasNextLine()) {
+                line = scan.nextLine();
                 if (line.endsWith(";")) {
                     line = line.substring(0, line.length() - 1);
                 }
@@ -35,22 +33,24 @@ public class Main {
                 while (rs.next()) {
                     int id = rs.getInt(1);
                     String name = rs.getString(2);
-                    Double price = rs.getDouble(3);
-                    System.out.println("Id= "  + id + " name= " + name + " price= " + price);
+                    double price = rs.getDouble(3);
+                    System.out.println("Id = " + id + " name = " + name + " price = " + price);
                 }
+
             } catch (SQLException ex) {
-                System.err.println("SQLEception message: " + ex.getMessage());
-                System.err.println("SQLException SQL state: " + ex.getSQLState());
-                System.err.println("SQLExcepton error code" + ex.getErrorCode());
+                System.err.println("SQLException message " + ex.getMessage());
+                System.err.println("SQLExceprion SQL state " + ex.getSQLState());
+                System.err.println("SQLException error code " + ex.getErrorCode());
             } finally {
-                if (rs !=null) {
+                if(rs != null) {
                     rs.close();
                 } else {
-                    System.out.println("Ошибка чтения данных с БД");
+                    System.err.println("Ошибка чтения данных с БД!");
                 }
             }
-
         }
-        }
-
+    }
 }
+
+
+
--- src/itvdn_jdbc/lesson_002/approach.sql
+++ src/itvdn_jdbc/lesson_002/approach.sql
@@ -1,11 +1,7 @@
-CREATE TABLE Books (
-  book_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  price DOUBLE, PRIMARY KEY (book_id));
-
-INSERT INTO Books (name, price) VALUE ('Inferno', 45.0);
-INSERT INTO Books (name, price) VALUE ('GHarry Potter', 45.5);
-INSERT INTO Books (name, price) VALUE ('It', 25.0);
-INSERT INTO Books (name, price) VALUE ('Spartacus', 55.0);
-INSERT INTO Books (name, price) VALUE ('Green Mile', 20.6);
-INSERT INTO Books (name, price) VALUE ('Solomon key', 5.0);
+CREATE TABLE Books (book_Id MEDIUMINT NOT NULL AUTO_INCREMENT, name VARCHAR(30) NOT NULL, price DOUBLE, PRIMARY KEY (book_Id));
+INSERT INTO Books (name, price) VALUES ('Inferno', 45.0);
+INSERT INTO Books (name, price) VALUES ('Harry Potter', 45.5);
+INSERT INTO Books (name, price) VALUES ('It', 25.0);
+INSERT INTO Books (name, price) VALUES ('Spartacus', 55.0);
+INSERT INTO Books (name, price) VALUES ('Green mile', 20.6);
+INSERT INTO Books (name, price) VALUES ('Solomon key', 5.0);
