9f24a5c4ff8881afe19d609cefd3422e57f731c9
kajuga <Ff14789kldd>
2019-04-24 19:42:38 +0300

    ITVDN JDBC course 24.04.19 add image in DB exercise

---
+++ src/itvdn_jdbc/lesson_003/BlobExample.java
@@ -0,0 +1,61 @@
+package itvdn_jdbc.lesson_003;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import java.io.OutputStream;
+import java.sql.*;
+
+public class BlobExample {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
+        String url = "jdbc:mysql://localhost:3306/first_lesson";
+        String userName = "root";
+        String pass = "pass";
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (Connection conn = DriverManager.getConnection(url, userName, pass);
+            Statement statement = conn.createStatement()) {
+            statement.executeUpdate("CREATE TABLE Images (name VARCHAR(15), d DATE, image BLOB)");
+
+            PreparedStatement preparedStatement = null;
+            try {
+
+                BufferedImage image = ImageIO.read(new File("/home/kajuga/projects/SQL/src/itvdn_jdbc/lesson_003/sudan.jpg"));
+                Blob nigga = conn.createBlob();
+                try(OutputStream outputStream = nigga.setBinaryStream(1)) {
+                    ImageIO.write(image, "jpg", outputStream);
+                }
+                preparedStatement = conn.prepareStatement("INSERT INTO Images (name, d, image) VALUES (?, {d ?}, ?)");
+                preparedStatement.setString(1, "NIGGA");
+                preparedStatement.setDate(2, Date.valueOf("2018-04-07"));
+                preparedStatement.setBlob(3, nigga);
+                preparedStatement.execute();
+
+                ResultSet resultSet = null;
+                try {
+                    resultSet = preparedStatement.executeQuery("SELECT * FROM Images");
+                    while (resultSet.next()) {
+                        Blob newNigga = resultSet.getBlob("image");
+                        BufferedImage image1 = ImageIO.read(newNigga.getBinaryStream());
+                        File outputFile = new File("saved.jpg");
+                        ImageIO.write(image1, "jpg", outputFile);
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    if(resultSet != null) {
+                        resultSet.close();
+                    } else {
+                        System.err.println("Ошибка чтения БД!!");
+                    }
+                }
+            } catch (SQLException exx) {
+                exx.printStackTrace();
+            } finally {
+                preparedStatement.close();
+            }
+        }
+    }
+}
---
+++ src/itvdn_jdbc/lesson_003/Main.java
@@ -0,0 +1,70 @@
+package itvdn_jdbc.lesson_003;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/first_lesson";
+        String userName = "root";
+        String pass = "pass";
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (Connection conn = DriverManager.getConnection(url, userName, pass)) {
+            PreparedStatement prepStat = null;
+            try {
+                prepStat = conn.prepareStatement("INSERT INTO Books (name, price) VALUES (?, ?)");
+                prepStat.setString(1, "Schindler`s list");
+                prepStat.setDouble(2, 33.5);
+                prepStat.execute();
+
+                ResultSet rs = null;
+                try {
+                    rs = prepStat.executeQuery("SELECT * FROM Books");
+                    while (rs.next()) {
+                        int id = rs.getInt(1);
+                        String name = rs.getString(2);
+                        double price = rs.getDouble(3);
+                        System.out.println("id= " + id + " name= " + name + " price= " + price);
+                    }
+                } catch (SQLException exc) {
+                    exc.printStackTrace();
+                } finally {
+                    if (rs != null) {
+                        rs.close();
+                    } else {
+                        System.err.println("Ошибка чтения с БД!");
+                    }
+                }
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                prepStat.close();
+            }
+
+
+            CallableStatement callableStatement = null;
+            try {
+                callableStatement = conn.prepareCall("{CALL booksCount(?)}");
+                callableStatement.registerOutParameter(1, Types.INTEGER);
+                callableStatement.execute();
+                System.out.println("Количество записей в таблице: " + callableStatement.getInt(1));
+            }catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                callableStatement.close();
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+}
