711f01f539c1edcdd6b36e9ebbd33a626efab99c
kajuga <kajuga@mail.ru>
2019-09-06 22:19:30 +0300

    jdbc

--- src/itvdn_jdbc/lesson_001/TestConnection.java
+++ src/itvdn_jdbc/lesson_001/TestConnection.java
@@ -1,5 +1,6 @@
 package itvdn_jdbc.lesson_001;
 
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,19 +8,13 @@ import java.sql.SQLException;
 public class TestConnection {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-        //задаю параментры подключения: URL, имя пользователя и пароль
-        String url = "jdbc:mysql://localhost:3306/first_lesson";
-        String userName = "root";
-        String pass = "pass";
-//Регистрация драйвера с помощью статического инициализатора
-        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/first_lesson?serverTimezone=EST5EDT";
+        String userName = "kajuga";
+        String pass = "ancd@1234";
+//        Class.forName("org.postgresql.Driver");
         try (Connection con = DriverManager.getConnection(url, userName, pass)) {
             System.out.println("Connection Successful");
 
         }
-
-
-
     }
 }
